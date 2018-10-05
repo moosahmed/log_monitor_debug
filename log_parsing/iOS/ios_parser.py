e21454f72e34e79bd0f98dfc3cde0f0c4167f6b1
Moosa Ahmed <moosahmed14@gmail.com>
2018-10-04 22:34:07 -0700

    ios parser

---
+++ log_parsing/iOS/ios_parser.py
@@ -0,0 +1,18 @@
+import re
+
+regex = re.compile(r'<property name="(.*?)">(.*?)</property>')
+
+out = []
+
+with open('sfbios.log', 'r') as f:
+    for line in f:
+        match = regex.search(line)
+        if match and match.group(2) == "Enabled":
+            out.append((match.group(1), match.group(2)))
+
+with open('filtered.txt', 'w') as f:
+    f.write("EXPORTED DATA:\n")
+    out_clean = list(set(out))
+    for item in out_clean:
+        print(item)
+        f.write(str(item) + "\n")
